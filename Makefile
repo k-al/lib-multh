bdeb0ae78c78052e203c81309e9ff67fe77e68ad
k-al <kai_a@t-online.de>
2022-01-21 19:02:28 +0100

    map is now compilable and usable for first tests. probably still a lot of bugs, but i dont have the motivation to write a propper unit test for it right now. also reworked the makefile to handle specific testing of one module.

--- Makefile
+++ Makefile
@@ -1,16 +1,34 @@
 CFLAGS = -std=c++17 -O3 -pthread -Wall
 
-ALL_TESTS = tests/Listworker_t01.app tests/Listworker_t02.app
-
 clean:
 	rm ./tests/*.app
 
-test: $(ALL_TESTS)
+test-map: tests/Map_t01.app
+	#
+	#
+	#
+	# Test:  ---  Map  ---
+	#
+	./tests/Map_t01.app
+
+test-listworker: tests/Listworker_t01.app tests/Listworker_t02.app
+	#
+	#
+	#
+	# Test:  ---  Listworker  ---
+	#
 	./tests/Listworker_t01.app
+	#
 	./tests/Listworker_t02.app
 
+test: test-listworker test-map
+
 tests/Listworker_t01.app: tests/listworker_t01.cpp lib/multh_listworker.hpp
+	echo "Test:  ---  Listworker_t01  ---"
 	g++ $(CFLAGS) -I./lib/ -o tests/Listworker_t01.app tests/listworker_t01.cpp
 
 tests/Listworker_t02.app: tests/listworker_t02.cpp lib/multh_listworker.hpp
 	g++ $(CFLAGS) -I./lib/ -o tests/Listworker_t02.app tests/listworker_t02.cpp
+
+tests/Map_t01.app: tests/map_t01.cpp lib/multh_map.hpp
+	g++ $(CFLAGS) -I./lib/ -o tests/Map_t01.app tests/map_t01.cpp
--- lib/multh_map.hpp
+++ lib/multh_map.hpp
@@ -3,22 +3,40 @@
 
 #include <vector>
 #include <utility>
+#include <cstring>
+#include <type_traits>
 
 
 // multithreading
 #include <mutex>
 #include <shared_mutex>
-#include <atomic>
 
 // for debugging
 // #include <iostream>
 
 namespace multh {
     
-    template<typename Key_Type, typename Data_Type>
+    template<typename First, typename Second>
+    struct pair {
+        static_assert(static_cast<bool>(std::is_trivially_copyable_v<First>), "multh::Map: ERROR!\n \tKey_Type requires trvially copying!\n");
+        static_assert(static_cast<bool>(std::is_trivially_copyable_v<Second>), "multh::Map: ERROR!\n \tKey_Type requires trvially copying!\n");
+        First first;
+        Second second;
+        
+        pair () = default;
+        
+        pair (pair&) = default;
+        
+        pair (First first, Second second) {
+            this->first = first;
+            this->second = second;
+        }
+    };
     
+    template<typename Key_Type, typename Data_Type>
     class Bucket {
-        using Element = std::pair<Key_Type, Data_Type*>;
+        static_assert(static_cast<bool>(std::is_trivially_copyable_v<Key_Type>), "multh::Map: ERROR!\n \tKey_Type requires trvially copying!\n");
+        using Element = pair<Key_Type, Data_Type*>;
     public:
         // number of valid elements in the list
         size_t size;
@@ -27,7 +45,7 @@ namespace multh {
         size_t capacity;
         
         // raw c-style array for the data
-        Element data[];
+        Element* data;
         
         // initialising without array allocation
         Bucket () {
@@ -61,16 +79,16 @@ namespace multh {
             this->size++;
             
             // check if reallocation of the memory must happen
-            if (this->size == this->capacity) {
+            if (this->size >= this->capacity) {
                 // increase the capacity linear
                 this->capacity += 2;
                 
-                const Element[] old_data = this->data;
+                const Element* old_data = this->data;
                 this->data = new Element[this->capacity];
                 
                 if (old_data) { // if old_data was actually used
-                    std::memcpy(this->data, old_data, this->size * size_of(Element));
-                    delete[](old_data, this->size);
+                    std::memcpy(this->data, old_data, this->size * sizeof(Element));
+                    delete[] old_data;
                 }
             }
             
@@ -95,7 +113,7 @@ namespace multh {
             }
             
             // save the data pointer before overwriting the element
-            const Data_Type* res = it->second;
+            Data_Type* res = it->second;
             
             // switch the found element to the end
             const Element* last = end - 1;
@@ -120,7 +138,7 @@ namespace multh {
             }
             return it->second;
         }
-    }
+    };
     
     
     
@@ -133,17 +151,23 @@ namespace multh {
         std::vector<std::mutex> bucket_mutex;
         std::vector<Bucket<Key_Type, Data_Type>> buckets;
         
+        Map () {
+            buckets = std::vector<Bucket<Key_Type, Data_Type>>(8);
+            bucket_mutex = std::vector<std::mutex>(8);
+        }
+        
         // get the data belonged to *key*
         inline Data_Type* get (Key_Type key) {
-            return this->get(key, std::uinque_lock());
+            std::unique_lock<std::mutex> tmp_lck = std::unique_lock<std::mutex>();
+            return this->get(key, tmp_lck);
         }
         
         // get the data belonged to *key* and keep the bucket locked with the *guard*
-        Data_Type* get (Key_Type key, std::unique_lock& guard) {
+        Data_Type* get (Key_Type key, std::unique_lock<std::mutex>& guard) {
             size_t hash = hash_function(key);
             
             std::shared_lock<std::shared_mutex> shared_lock_guard(this->rehash_mtx);
-            hash = hash % this->buckets.size;
+            hash = hash % this->buckets.size();
             
             guard = std::unique_lock(this->bucket_mutex[hash]);
             return buckets[hash].get(key);
@@ -154,20 +178,21 @@ namespace multh {
             size_t hash = hash_function(key);
             
             std::shared_lock<std::shared_mutex> shared_lock_guard(this->rehash_mtx);
-            hash = hash % this->buckets.size;
+            hash = hash % this->buckets.size();
             
             std::unique_lock bucket_lock_guard(this->bucket_mutex[hash]);
-            return buckets[hash].add(std::pair<Key_Type, Data_Type*>(key, data));
+            multh::pair<Key_Type, Data_Type*> new_el(key, data);
+            return buckets[hash].add(new_el);
         }
         
         bool erease (Key_Type key) {
             size_t hash = hash_function(key);
             
             std::shared_lock<std::shared_mutex> shared_lock_guard(this->rehash_mtx);
-            hash = hash % this->buckets.size;
+            hash = hash % this->buckets.size();
             
             std::unique_lock bucket_lock_guard(this->bucket_mutex[hash]);
-            Data_Type* ptr = buckets[hash].del(std::pair<Key_Type, Data_Type*>(key, data));
+            Data_Type* ptr = buckets[hash].del(key);
             
             if (ptr) {
                 delete(ptr);
@@ -176,7 +201,7 @@ namespace multh {
                 return false;
             }
         }
-    }
+    };
 }
 
 #endif
